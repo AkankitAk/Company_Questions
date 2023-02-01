a65fd3b20a7cb09c518234d64bda96d17a6d99af
Ankit <akankitag@gmail.com>
2023-02-02 03:06:55 +0530

    Question

---
+++ MountBlue_Technologies/Question_4.java
@@ -0,0 +1,14 @@
+package MountBlue_Technologies;
+/* 
+    Question 3:- Merging two unsorted arrays in sorted order.
+    Eg:- Input: a[] = {10, 5, 15}
+    b[] = {20, 3, 2}
+    Output : Merge List :
+    {2, 3, 5, 10, 15, 20}
+    
+    https://www.geeksforgeeks.org/merging-two-unsorted-arrays-sorted
+*/
+
+public class Question_4 {
+
+}
